702424849b8cf1c9202fe8b12b4531416df44d99
larineoy <larineoyhan@gmail.com>
2026-04-18 12:10:00 -0400

    added __init__.py

---
+++ inference/__init__.py
@@ -0,0 +1,2 @@
+from inference.predictor import Predictor
+from inference.colorize import colorize_mask
---
+++ training/__init__.py
@@ -0,0 +1,3 @@
+from training.loss import SegLoss
+from training.metrics import compute_miou, compute_pixel_acc
+from training.trainer import Trainer
